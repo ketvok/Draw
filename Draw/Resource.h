47e68428c45e141e8e6399e85e3aadf68d041d1f
Filip Leš <files@vsite.hr>
2025-06-11 22:34:20 +0200

    Add shape resource and update ribbon

--- Draw/Resource.h
+++ Draw/Resource.h
@@ -22,17 +22,18 @@
 #define IDR_WINDOWS_MENU                152
 #define IDS_EDIT_MENU                   306
 #define IDR_RIBBON                      307
+#define IDB_SHAPES                      310
 #define ID_WRITE_PASTEASHYPERLINK       32770
-#define ID_BUTTON2                      32771
-#define ID_BUTTON3                      32772
-#define ID_BUTTON4                      32773
+#define ID_GALLERY_BUTTON               32771
+#define ID_BUTTON_ERASER                32774
+#define ID_BUTTON_PEN                   32775
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        310
-#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_RESOURCE_VALUE        311
+#define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
